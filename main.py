209967a9af17d9d7e11b2914f291b76a3919dab3
jm1021 <jmort1021@gmail.com>
2025-05-13 11:12:05 -0700

    finish the titanic intergration

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ from api.nestPost import nestPost_api # Justin added this, custom format for his
 from api.messages_api import messages_api # Adi added this, messages for his website
 from api.carphoto import car_api
 from api.carChat import car_chat_api
+from api.titanic import titanic_api # Titanic API endpoint
 
 from api.vote import vote_api
 # database Initialization functions
@@ -37,6 +38,7 @@ from model.channel import Channel, initChannels
 from model.post import Post, initPosts
 from model.nestPost import NestPost, initNestPosts # Justin added this, custom format for his website
 from model.vote import Vote, initVotes
+from model.titanic import TitanicModel, initTitanic # Titanic model for prediction
 # server only Views
 
 # register URIs for api endpoints
@@ -53,6 +55,7 @@ app.register_blueprint(nestPost_api)
 app.register_blueprint(nestImg_api)
 app.register_blueprint(vote_api)
 app.register_blueprint(car_api)
+app.register_blueprint(titanic_api) # Titanic API endpoint
 
 # Tell Flask-Login the view function name of your login route
 login_manager.login_view = "login"
